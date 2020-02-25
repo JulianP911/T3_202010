ed68cb4c8a202e5c30fac5cec62634a1272f055a
ppastram <ppastram@hotmail.com>
2020-02-24 22:53:04 -0500

    Tests

--- test/model/logic/ModeloTest.java
+++ test/model/logic/ModeloTest.java
@@ -17,6 +17,9 @@ class ModeloTest
 	 */
 	private LinkedList<Integer> lista1;
 	
+	private LinkedList<Double> lista2;
+
+	
 	/*
 	 * Escenario 1: Crea una lista sencilla de tipo integer
 	 */
@@ -29,6 +32,54 @@ class ModeloTest
 		lista1.addNodeLast(3);
 		lista1.addNodeLast(4);
 	}
+	
+	public void setupEscenarioAleatorio( )
+	{
+		lista2 = new LinkedList<Double>();
+		
+		lista2.addNodeLast( 7.0 );
+		lista2.addNodeLast( 5.0 );
+		lista2.addNodeLast( 4.0 );
+		lista2.addNodeLast( 3.0 );
+		lista2.addNodeLast( 2.0 );
+
+
+		for (int i = 0; i < 90; i++)
+		{
+			lista2.addNodeLast( Math.random() );
+		}
+		
+		lista2.addNodeLast( -1.0 );
+		lista2.addNodeLast( -2.0 );
+		lista2.addNodeLast( -3.0 );
+		lista2.addNodeLast( -4.0 );
+		lista2.addNodeLast( -5.0 );
+
+
+	}
+	
+	public void setupEscenarioAsc( )
+	{
+		lista1 = new LinkedList<Integer>();
+		
+		for (int i = 0; i < 100; i++)
+		{
+			lista1.addNodeLast( i );
+		}
+
+	}
+	
+	public void setupEscenarioDes( )
+	{
+		lista1 = new LinkedList<Integer>();
+		
+		for (int i = 100; i > 0; i--)
+		{
+			lista1.addNodeLast( i );
+		}
+
+	}
+	
 	
 	/**
 	 * Prueba 1: Verifica el tamaño de comparendos sea correcto
@@ -67,17 +118,156 @@ class ModeloTest
 	@Test
 	void testShellSort()
 	{
-		fail("Not yet implemented");
+		setupEscenarioAleatorio();
+		
+		
+		assertEquals(100, lista2.getSize());
+
+		assertEquals(-5, lista2.getFirst().getItem() );
+		assertEquals(-4, lista2.getFirst().getItem() );
+		assertEquals(-3, lista2.getFirst().getItem() );
+		assertEquals(-2, lista2.getFirst().getItem() );
+		assertEquals(-1, lista2.getFirst().getItem() );
+		
+		for (int i = 0; i < 90; i++)
+		{
+			lista2.getFirst();
+		}
+		
+		assertEquals(2, lista2.getFirst().getItem() );
+		assertEquals(3, lista2.getFirst().getItem() );
+		assertEquals(4, lista2.getFirst().getItem() );
+		assertEquals(5, lista2.getFirst().getItem() );
+		assertEquals(7, lista2.getFirst().getItem() );
+
+		
+		
+		
+		setupEscenarioAsc();
+		
+		assertEquals(100, lista1.getSize());
+		
+		for (int i = 0; i < 100; i++)
+		{
+			assertEquals(i, lista1.getFirst().getItem() );
+		}
+		
+		
+		
+		
+		setupEscenarioDes();
+		
+		assertEquals(100, lista1.getSize());
+		
+		for (int i = 100; i > 0; i--)
+		{
+			assertEquals(i, lista1.getFirst().getItem() );
+		}
+
 	}
 
 	@Test
-	void testQuickSort() {
-		fail("Not yet implemented");
+	void testQuickSort() 
+	{
+		setupEscenarioAleatorio();
+		
+		
+		assertEquals(100, lista2.getSize());
+
+		assertEquals(-5, lista2.getFirst().getItem() );
+		assertEquals(-4, lista2.getFirst().getItem() );
+		assertEquals(-3, lista2.getFirst().getItem() );
+		assertEquals(-2, lista2.getFirst().getItem() );
+		assertEquals(-1, lista2.getFirst().getItem() );
+		
+		for (int i = 0; i < 90; i++)
+		{
+			lista2.getFirst();
+		}
+		
+		assertEquals(2, lista2.getFirst().getItem() );
+		assertEquals(3, lista2.getFirst().getItem() );
+		assertEquals(4, lista2.getFirst().getItem() );
+		assertEquals(5, lista2.getFirst().getItem() );
+		assertEquals(7, lista2.getFirst().getItem() );
+
+		
+		
+		
+		setupEscenarioAsc();
+		
+		assertEquals(100, lista1.getSize());
+		
+		for (int i = 0; i < 100; i++)
+		{
+			assertEquals(i, lista1.getFirst().getItem() );
+		}
+		
+		
+		
+		
+		setupEscenarioDes();
+		
+		assertEquals(100, lista1.getSize());
+		
+		for (int i = 100; i > 0; i--)
+		{
+			assertEquals(i, lista1.getFirst().getItem() );
+		}
+
 	}
 
+
 	@Test
-	void testMerge() {
-		fail("Not yet implemented");
+	void testMerge() 
+	{
+		setupEscenarioAleatorio();
+		
+		
+		assertEquals(100, lista2.getSize());
+
+		assertEquals(-5, lista2.getFirst().getItem() );
+		assertEquals(-4, lista2.getFirst().getItem() );
+		assertEquals(-3, lista2.getFirst().getItem() );
+		assertEquals(-2, lista2.getFirst().getItem() );
+		assertEquals(-1, lista2.getFirst().getItem() );
+		
+		for (int i = 0; i < 90; i++)
+		{
+			lista2.getFirst();
+		}
+		
+		assertEquals(2, lista2.getFirst().getItem() );
+		assertEquals(3, lista2.getFirst().getItem() );
+		assertEquals(4, lista2.getFirst().getItem() );
+		assertEquals(5, lista2.getFirst().getItem() );
+		assertEquals(7, lista2.getFirst().getItem() );
+
+		
+		
+		
+		setupEscenarioAsc();
+		
+		assertEquals(100, lista1.getSize());
+		
+		for (int i = 0; i < 100; i++)
+		{
+			assertEquals(i, lista1.getFirst().getItem() );
+		}
+		
+		
+		
+		
+		setupEscenarioDes();
+		
+		assertEquals(100, lista1.getSize());
+		
+		for (int i = 100; i > 0; i--)
+		{
+			assertEquals(i, lista1.getFirst().getItem() );
+		}
+
 	}
+
 
 }
